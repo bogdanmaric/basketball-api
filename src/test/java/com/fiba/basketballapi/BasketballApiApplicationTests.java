744c8ff8325dd6d07ce97075e274bd5565020388
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2023-11-14 21:09:18 +0100

    Initial commit: Basketball API project

---
+++ src/test/java/com/fiba/basketballapi/BasketballApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiba.basketballapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
